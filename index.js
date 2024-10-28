16fda70662046024c9bc5e6398b700d9865d1876
BritanyLM <britanylm@gmail.com>
2024-10-28 07:29:29 -0400

    second commit

--- index.js
+++ index.js
@@ -14,23 +14,49 @@ const getAllPlayers = async () => {
       }
         };
 
+        const rendersinglePlayer = async (playerId) => {
+            try {
+                const response = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2409-FTB-ET-WEB-FT/players/${playerId}`);
+                const playerDetails = await response.json();
+                console.log('Player Details:', playerDetails);
+            } catch (error) {
+                console.error('error:', error);
+            }
+        };
+           
+                  
+
     getAllPlayers().then(response => {
     if (response && response.success) {
       console.log('All players:', response.data.players);
       const players = response.data.players;
+      
       if (Array.isArray(players)) {
         const playerLis = players.map(singlePlayer => {
-            return `<li>${singlePlayer.name}</li>`;
-        
-        
-    
+            return `<li data-id="${singlePlayer.id}">${singlePlayer.name}</li>`;
+            
+        });
 
-});
+
+
+    const ol = document.createElement(`ol`);
+    ol.innerHTML = playerLis.join(``);
+    document.body.appendChild(ol);
       
-const ol = document.createElement(`ol`);
-ol.innerHTML = playerLis.join(``);
-document.body.appendChild(ol);
-      }
-    }
+    const Lis = ol.querySelectorAll('li');
 
-    })
+    Lis.forEach(singlePlayerLi => {
+    singlePlayerLi.addEventListener('click', async (event) => {
+        const playerID = event.currentTarget.getAttribute('data-id');
+        console.log('Player ID:', playerID); 
+        if (playerID) { 
+            await rendersinglePlayer(playerID);
+        } else {
+            console.error('Player ID is null');
+        }
+        
+        });
+    });
+}
+}
+    });
